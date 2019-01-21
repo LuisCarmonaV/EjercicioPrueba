6f7c98f092c8bce722881d17ec61e589ed0acc49
LuisCarmonaV <46898166+LuisCarmonaV@users.noreply.github.com>
2019-01-21 09:41:39 -0600

    Luis Adrian Carmona Villalobos, Grupo 02

---
+++ VelocidadPromedio.py
@@ -0,0 +1,9 @@
+#Autor: Luis Adrian Carmona Villalobos
+
+
+tiempo = int(input("teclea el tiempo del viaje en hrs. enteras: "))
+distancia = int(input("teclea la distancia del viaje en km enteros: "))
+
+velocidad = distancia / tiempo
+
+print("La velocidad promedio es: ", velocidad, "km/h")
